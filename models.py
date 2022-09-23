03b8d93ab68d6167b6d77aee32bdf5f2a7e609b4
JuliaXxj <43887604+JuliaXxj@users.noreply.github.com>
2022-09-23 12:12:53 -0400

    Add two files

---
+++ models.py
@@ -0,0 +1,354 @@
+import torch
+import torch.nn as nn
+import torch.nn.functional as F
+import torch.optim as optim
+from torchvision import datasets, transforms
+from torch.optim.lr_scheduler import StepLR
+import numpy as np
+import logging
+from datetime import datetime
+import copy
+
+# to get activation
+ACTIVATION = None
+
+
+def get_activation(name, tensor_logger, detach, is_lastlayer=False):
+    if is_lastlayer:
+        def hook(model, input, output):
+            raw = torch.flatten(output, start_dim=1, end_dim=-1).cpu().detach().numpy()
+            # use argmax instead of broadcasting just in case comparing floating point is finicky
+
+            mask = np.zeros(raw.shape, dtype=bool)
+
+            mask[np.arange(raw.shape[0]), raw.argmax(axis=1)] = 1
+
+            tensor_logger[name] = np.concatenate((tensor_logger[name], mask),
+                                                 axis=0) if name in tensor_logger else mask
+
+        return hook
+
+    if detach:
+        def hook(model, input, output):
+            raw = torch.flatten(
+                output, start_dim=1, end_dim=-1).cpu().detach().numpy()
+            raw = raw > 0
+            logging.debug("{}, {}".format(name, raw.shape))
+            tensor_logger[name] = np.concatenate((tensor_logger[name], raw),
+                                                 axis=0) if name in tensor_logger else raw
+            logging.debug(tensor_logger[name].shape)
+
+        return hook
+    else:
+        # keep the gradient, so cannot convert to bit here
+        def hook(model, input, output):
+            raw = torch.sigmoid(torch.flatten(
+                output, start_dim=1, end_dim=-1))
+            logging.debug("{}, {}".format(name, raw.shape))
+            tensor_logger[name] = torch.cat((tensor_logger[name], raw),
+                                            axis=0) if name in tensor_logger else raw
+            logging.debug(tensor_logger[name].shape)
+
+        return hook
+
+
+def get_gradient(name, gradient_logger, detach):
+    def hook(model, grad_input, grad_output):
+        raw = grad_output
+        assert (len(raw) == 1)
+        raw = raw[0].cpu().detach().numpy()
+        gradient_logger[name] = np.concatenate((gradient_logger[name], raw), axis=0) if name in gradient_logger else raw
+
+    return hook
+
+
+class BaseNet(nn.Module):
+    def __init__(self):
+        super(BaseNet, self).__init__()
+        self.tensor_log = {}
+        self.gradient_log = {}
+        self.hooks = []
+        self.bw_hooks = []
+
+    def reset_hooks(self):
+        self.tensor_log = {}
+        for h in self.hooks:
+            h.remove()
+
+    def reset_bw_hooks(self):
+        self.input_labels = None
+        self.gradient_log = {}
+        for h in self.bw_hooks:
+            h.remove()
+
+    def register_log(self, detach):
+        raise NotImplementedError
+
+    def register_gradient(self, detach):
+        raise NotImplementedError
+
+    def model_savename(self):
+        raise NotImplementedError
+
+    def get_pattern(self, input, layers, device, flatten=True):
+        self.eval()
+        self.register_log()
+        self.forward(input.to(device))
+        tensor_log = copy.deepcopy(self.tensor_log)
+        if flatten:
+            return np.concatenate([tensor_log[l] for l in layers], axis=1)
+        return tensor_log
+
+
+class TinyCNN(BaseNet):
+    def __init__(self):
+        super(TinyCNN, self).__init__()
+        self.conv1 = nn.Conv2d(1, 4, 3, 1)
+        self.conv2 = nn.Conv2d(4, 8, 3, 1)
+        self.dropout1 = nn.Dropout(0.25)
+        self.dropout2 = nn.Dropout(0.5)
+        self.fc1 = nn.Linear(1152, 16)
+        self.fc2 = nn.Linear(16, 10)
+
+    def forward(self, x):
+        x = self.conv1(x)
+        x = F.relu(x)
+        x = self.conv2(x)
+        x = F.relu(x)
+        x = F.max_pool2d(x, 2)
+        #         x = self.dropout1(x)
+        x = torch.flatten(x, 1)
+        # print(x.shape)
+        x = self.fc1(x)
+        x = F.relu(x)
+        #         x = self.dropout2(x)
+        x = self.fc2(x)
+        output = F.log_softmax(x, dim=1)
+        return output
+
+    def register_log(self, detach=True):
+        self.reset_hooks()
+        # first layer should not make any difference?
+        self.hooks.append(self.conv1.register_forward_hook(get_activation('conv1', self.tensor_log, detach)))
+        self.hooks.append(self.conv2.register_forward_hook(get_activation('conv2', self.tensor_log, detach)))
+        self.hooks.append(self.fc1.register_forward_hook(get_activation('fc1', self.tensor_log, detach)))
+        self.hooks.append(self.fc2.register_forward_hook(get_activation('fc2', self.tensor_log, detach)))
+
+    def register_gradient(self, detach=True):
+        self.reset_bw_hooks()
+        # first layer should not make any difference?
+        self.bw_hooks.append(self.conv1.register_backward_hook(get_gradient('conv1', self.gradient_log, detach)))
+        self.bw_hooks.append(self.conv2.register_backward_hook(get_gradient('conv2', self.gradient_log, detach)))
+        self.bw_hooks.append(self.fc1.register_backward_hook(get_gradient('fc1', self.gradient_log, detach)))
+        self.bw_hooks.append(self.fc2.register_backward_hook(get_gradient('fc2', self.gradient_log, detach)))
+
+    def model_savename(self, tag=""):
+        return "TinyCNN" + tag + datetime.now().strftime("%H-%M-%S")
+
+
+class FeedforwardNeuralNetModel(BaseNet):
+    def __init__(self, input_dim, hidden_dim, output_dim):
+        super(FeedforwardNeuralNetModel, self).__init__()
+
+        # Linear function
+        self.fc1 = nn.Linear(input_dim, 256)
+        self.fc2 = nn.Linear(256, 128)
+        self.fc3 = nn.Linear(128, 64)
+        self.fc4 = nn.Linear(64, output_dim)
+
+    def register_log(self, detach=True):
+        self.reset_hooks()
+        # first layer should not make any difference?
+        self.hooks.append(self.fc1.register_forward_hook(get_activation('fc1', self.tensor_log, detach)))
+        self.hooks.append(self.fc2.register_forward_hook(get_activation('fc2', self.tensor_log, detach)))
+        self.hooks.append(self.fc3.register_forward_hook(get_activation('fc3', self.tensor_log, detach)))
+        # self.hooks.append(self.fc4.register_forward_hook(get_activation('fc4', self.tensor_log, detach)))
+        self.hooks.append(
+            self.fc4.register_forward_hook(get_activation('fc4', self.tensor_log, detach, is_lastlayer=True)))
+
+    def register_gradient(self, detach=True):
+        self.reset_bw_hooks()
+        # first layer should not make any difference?
+        self.bw_hooks.append(self.fc1.register_backward_hook(get_gradient('fc1', self.gradient_log, detach)))
+        self.bw_hooks.append(self.fc2.register_backward_hook(get_gradient('fc2', self.gradient_log, detach)))
+        self.bw_hooks.append(self.fc3.register_backward_hook(get_gradient('fc3', self.gradient_log, detach)))
+        self.bw_hooks.append(self.fc4.register_backward_hook(get_gradient('fc4', self.gradient_log, detach)))
+
+    def forward(self, x):
+        out = F.relu(self.fc1(x.view(-1, 28 * 28)))
+        out = F.relu(self.fc2(out))
+        out = F.relu(self.fc3(out))
+        out = self.fc4(out)
+        #    out = F.log_softmax(out, dim=1)
+        return out
+
+    def model_savename(self):
+        return "FFN" + datetime.now().strftime("%H-%M-%S")
+
+
+class NewFeedforwardNeuralNetModel(BaseNet):
+    def __init__(self, input_dim, hidden_dim, output_dim):
+        super(NewFeedforwardNeuralNetModel, self).__init__()
+
+        # Linear function
+        self.fc1 = nn.Linear(input_dim, 256)
+        self.fc2 = nn.Linear(256, 128)
+        self.fc3 = nn.Linear(128, 64)
+        self.fc4 = nn.Linear(64, output_dim)
+
+    def register_log(self, detach=True):
+        self.reset_hooks()
+        # first layer should not make any difference?
+        self.hooks.append(self.fc1.register_forward_hook(get_activation('fc1', self.tensor_log, detach)))
+        self.hooks.append(self.fc2.register_forward_hook(get_activation('fc2', self.tensor_log, detach)))
+        self.hooks.append(self.fc3.register_forward_hook(get_activation('fc3', self.tensor_log, detach)))
+        # self.hooks.append(self.fc4.register_forward_hook(get_activation('fc4', self.tensor_log, detach)))
+        self.hooks.append(
+            self.fc4.register_forward_hook(get_activation('fc4', self.tensor_log, detach, is_lastlayer=True)))
+
+    def register_gradient(self, detach=True):
+        self.reset_bw_hooks()
+        # first layer should not make any difference?
+        self.bw_hooks.append(self.fc1.register_backward_hook(get_gradient('fc1', self.gradient_log, detach)))
+        self.bw_hooks.append(self.fc2.register_backward_hook(get_gradient('fc2', self.gradient_log, detach)))
+        self.bw_hooks.append(self.fc3.register_backward_hook(get_gradient('fc3', self.gradient_log, detach)))
+        self.bw_hooks.append(self.fc4.register_backward_hook(get_gradient('fc4', self.gradient_log, detach)))
+
+    def forward(self, x):
+        out = F.relu(self.fc1(x.view(-1, 28 * 28)))
+        out = F.relu(self.fc2(out))
+        out = F.relu(self.fc3(out))
+        out = self.fc4(out)
+        out = F.log_softmax(out, dim=1)
+        return out
+
+    def model_savename(self):
+        return "NFFN" + datetime.now().strftime("%H-%M-%S")
+
+class PatternClassifier(BaseNet):
+    def __init__(self, input_dim, max_unit, output_dim):
+        super(PatternClassifier, self).__init__()
+        self.max_unit = max_unit
+        # Linear function
+        self.fc1 = nn.Linear(self.max_unit, output_dim, bias=False)
+        # self.fc2 = nn.Linear(hidden_dim, output_dim)
+
+    def register_log(self):
+        self.reset_hooks()
+        # first layer should not make any difference?
+        self.hooks.append(self.fc1.register_forward_hook(get_activation('fc1', self.tensor_log)))
+        # self.hooks.append(self.fc2.register_forward_hook(get_activation('fc2', self.tensor_log)))
+
+    def forward(self, x):
+        # out = F.relu(self.fc1(x))
+        out = self.fc1(x[:, :self.max_unit])
+        # out = self.fc2(out)
+        out = F.log_softmax(out, dim=1)
+        return out
+
+    def model_savename(self):
+        return "PatternClasffier" + datetime.now().strftime("%H:%M:%S")
+
+
+def fgsm_attack(image, eps, data_grad):
+    sign_data_grad = data_grad.sign()
+    pertubed_image = image + eps * sign_data_grad
+    pertubed_image = torch.clamp(pertubed_image, 0, 1)
+    return pertubed_image
+
+
+
+
+# https://github.com/pytorch/examples/tree/main/mnist
+
+class NewTinyCNN(BaseNet):
+    def __init__(self):
+        super(NewTinyCNN, self).__init__()
+        self.conv1 = nn.Conv2d(1, 32, 3, 1)
+        self.conv2 = nn.Conv2d(32, 64, 3, 1)
+        self.dropout1 = nn.Dropout(0.25)
+        self.dropout2 = nn.Dropout(0.5)
+        self.fc1 = nn.Linear(9216, 128)
+        self.fc2 = nn.Linear(128, 10)
+
+    def forward(self, x):
+        x = self.conv1(x)
+        x = F.relu(x)
+        x = self.conv2(x)
+        x = F.relu(x)
+        x = F.max_pool2d(x, 2)
+        #         x = self.dropout1(x)
+        x = torch.flatten(x, 1)
+        # print(x.shape)
+        x = self.fc1(x)
+        x = F.relu(x)
+        #         x = self.dropout2(x)
+        x = self.fc2(x)
+        output = F.log_softmax(x, dim=1)
+        return output
+
+    def register_log(self, detach=True):
+        self.reset_hooks()
+        # first layer should not make any difference?
+        self.hooks.append(self.conv1.register_forward_hook(get_activation('conv1', self.tensor_log, detach)))
+        self.hooks.append(self.conv2.register_forward_hook(get_activation('conv2', self.tensor_log, detach)))
+        self.hooks.append(self.fc1.register_forward_hook(get_activation('fc1', self.tensor_log, detach)))
+        self.hooks.append(self.fc2.register_forward_hook(get_activation('fc2', self.tensor_log, detach)))
+
+    def register_gradient(self, detach=True):
+        self.reset_bw_hooks()
+        # first layer should not make any difference?
+        self.bw_hooks.append(self.conv1.register_backward_hook(get_gradient('conv1', self.gradient_log, detach)))
+        self.bw_hooks.append(self.conv2.register_backward_hook(get_gradient('conv2', self.gradient_log, detach)))
+        self.bw_hooks.append(self.fc1.register_backward_hook(get_gradient('fc1', self.gradient_log, detach)))
+        self.bw_hooks.append(self.fc2.register_backward_hook(get_gradient('fc2', self.gradient_log, detach)))
+
+    def model_savename(self, tag=""):
+        return "NewTinyCNN" + tag + datetime.now().strftime("%H:%M:%S")
+
+class MNISTNet(nn.Module):
+    def __init__(self):
+        super(MNISTNet, self).__init__()
+        self.conv1 = nn.Conv2d(1, 32, 3, 1)
+        self.conv2 = nn.Conv2d(32, 64, 3, 1)
+        self.dropout1 = nn.Dropout(0.25)
+        self.dropout2 = nn.Dropout(0.5)
+        self.fc1 = nn.Linear(9216, 128)
+        self.fc2 = nn.Linear(128, 10)
+
+    def forward(self, x):
+        x = self.conv1(x)
+        x = F.relu(x)
+        x = self.conv2(x)
+        x = F.relu(x)
+        x = F.max_pool2d(x, 2)
+        x = self.dropout1(x)
+        x = torch.flatten(x, 1)
+        x = self.fc1(x)
+        x = F.relu(x)
+        x = self.dropout2(x)
+        x = self.fc2(x)
+        output = F.log_softmax(x, dim=1)
+        return output
+
+
+# https://pytorch.org/tutorials/beginner/blitz/cifar10_tutorial.html
+class CIFAR10Net(nn.Module):
+    def __init__(self):
+        super().__init__()
+        self.conv1 = nn.Conv2d(3, 6, 5)
+        self.pool = nn.MaxPool2d(2, 2)
+        self.conv2 = nn.Conv2d(6, 16, 5)
+        self.fc1 = nn.Linear(16 * 5 * 5, 120)
+        self.fc2 = nn.Linear(120, 84)
+        self.fc3 = nn.Linear(84, 10)
+
+    def forward(self, x):
+        x = self.pool(F.relu(self.conv1(x)))
+        x = self.pool(F.relu(self.conv2(x)))
+        x = torch.flatten(x, 1)  # flatten all dimensions except batch
+        x = F.relu(self.fc1(x))
+        x = F.relu(self.fc2(x))
+        x = self.fc3(x)
+        # output = F.softmax(x, dim=1)
+        return x
---
+++ utils.py
@@ -0,0 +1,91 @@
+import shutil
+import imageio.v2 as imageio
+import os
+import matplotlib.pyplot as plt
+import numpy as np
+import torchvision
+import logging
+import copy
+import torch
+import torch.nn.functional as F
+
+def generate_gif(gif_filename, img_folder, image_names, delete_folder=False):
+    """
+    Generate the gif of the process of how the image gets transformed from all the images image_names saved in img_folder.
+    Save the generated gif file with name gif_filename.
+    Delete the img_folder if delete_folder is True.
+    """
+    with imageio.get_writer(gif_filename, mode='I') as writer:
+        for img_name in image_names:
+            image = imageio.imread(os.path.join(img_folder, img_name))
+            writer.append_data(image)
+
+    if delete_folder:
+        shutil.rmtree(img_folder)
+
+
+def cifar10_imshow(img, img_title=None, save_img=False, img_path=None):
+    img = torchvision.utils.make_grid(img)
+    img = img / 2 + 0.5  # unnormalize
+    npimg = img.numpy()
+    figure = plt.figure()
+    plt.title("Pred prob: {}".format(img_title))
+    plt.imshow(np.transpose(npimg, (1, 2, 0)))
+    if save_img:
+        plt.savefig(img_path)
+    plt.show()
+
+
+def cifar10_img(img):
+    """
+    Transform the image from cifar10 to a form that can be inputted for matplotlib.
+    """
+    img = torchvision.utils.make_grid(img)
+    # unnormalize the image, see the transform applied to img in train_cifar10.py.
+    img = img / 2 + 0.5
+    npimg = img.numpy()
+    return np.transpose(npimg, (1, 2, 0))
+
+
+def get_grad_each_label(gradient_log, target_log, layers, labels) -> dict:
+    """
+    return a dictionary of (k,v), in which k is one lable, v is a numpy array of all gradient for that particular label k
+    """
+    for l in layers:
+        logging.debug(gradient_log[l].shape)
+    res = {}
+    for label in labels:
+        res[label] = copy.deepcopy(np.concatenate([gradient_log[l][target_log == label] for l in layers],
+                                                  axis=1))
+        logging.debug(res[label].shape)
+
+    return res
+
+
+def test(model, device, test_loader, trace=False, detach=True):
+    model.eval()
+    test_loss = 0
+    correct = 0
+    if trace:
+        model.register_log(detach)
+    with torch.no_grad():
+        for data, target in test_loader:
+            data, target = data.to(device), target.to(device)
+            output = model(data.float())
+            # sum up batch loss
+            test_loss += F.nll_loss(F.log_softmax(output, dim=1), target, reduction='sum').item()
+            # get the index of the max log-probability
+            pred = output.argmax(dim=1, keepdim=True)
+            correct += pred.eq(target.view_as(pred)).sum().item()
+
+    test_loss /= len(test_loader.dataset)
+
+    if not trace:
+        print('\nTest set: Average loss: {:.4f}, Accuracy: {}/{} ({:.0f}%)\n'.format(
+            test_loss, correct, len(test_loader.dataset),
+            100. * correct / len(test_loader.dataset)))
+
+    tensor_log = copy.deepcopy(model.tensor_log)
+
+    model.reset_hooks()
+    return tensor_log
